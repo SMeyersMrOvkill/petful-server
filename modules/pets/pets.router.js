070795bd8e311e5daec17aef7e9acb37bb7c9c37
Samuel L Meyers <thecheesecakejunkie@gmail.com>
2021-01-22 11:59:00 -0600

    Initial commit

---
+++ modules/pets/pets.router.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const json = require('body-parser').json()
+
+const Pets = require('./pets.service')
+const People = require('../people/people.service')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  return res.json(Pets.get());
+})
+
+router.get('/cat', (req, res) => {
+  return res.json(Pets.getCat());
+})
+
+router.get('/dog', (req, res) => {
+  return res.json(Pets.getDog());
+})
+
+router.delete('/', json, (req, res) => {
+  const { type } = req.body;
+  if(!type) {
+    return res.status(400).json({status: -1});
+  }
+  if(type.toLowerCase() !== 'dog' && type.toLowerCase() !== 'cat') {
+    return res.status(400).json({status: -2});
+  }
+  Pets.dequeue(type);
+  return res.status(201).json({status: 0});
+})
+
+
+module.exports = router
---
+++ modules/pets/pets.service.js
@@ -0,0 +1,42 @@
+const Queue = require('../queue/Queue')
+const store = require('../../store')
+
+// Set up initial data.
+// --------------------
+
+const pets = {
+  cats: new Queue(),
+  dogs: new Queue()
+}
+
+store.cats.forEach(cat => pets.cats.enqueue(cat))
+store.dogs.forEach(dog => pets.dogs.enqueue(dog))
+
+// --------------------
+
+module.exports = {
+  get() {
+    return {
+      cat: pets.cats.peek(),
+      dog: pets.dogs.peek()
+    };
+  },
+
+  getDog() {
+    return pets.dogs.peek();
+  },
+
+  getCat() {
+    return pets.cats.peek();
+  },
+
+  dequeue(type) {
+    if(type.toLowerCase() === 'dog') {
+      pets.dogs.dequeue();
+    } else if(type.toLowerCase() === 'cat') {
+      pets.cats.dequeue();
+    } else {
+      console.log('Error: We only have cats and dogs. [' + type + ']');
+    }
+  }
+}
